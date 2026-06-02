e79a5567473a1aae178f62b7316110f5a3b89f21
VisheshPanwar2003 <142052984+VisheshPanwar2003@users.noreply.github.com>
2026-06-02 15:45:08 +0530

    Update vision_service.py

--- services/vision_service.py
+++ services/vision_service.py
@@ -1,73 +1,24 @@
-import os
-
-import google.generativeai as genai
-
-
-genai.configure(
-
-    api_key=os.getenv(
-        "GEMINI_API_KEY"
-    )
-
-)
-
-
-model = genai.GenerativeModel(
-
-    "gemini-2.5-flash"
-
-)
-
-
 def analyze_image_question(
-
     image,
-
     question
-
 ):
 
     try:
 
         response = model.generate_content([
-
             question,
-
             image
-
         ])
 
+        print(response)
 
-        if not response:
-
-            return "No response generated."
-
-
-        if hasattr(
-
-            response,
-
-            "text"
-
-        ):
-
-            return response.text
-
-
-        return str(response)
+        return response.text
 
     except Exception as e:
 
         print(
-
-            "GEMINI ERROR:",
-
-            e
-
+            "GEMINI FAILED:",
+            str(e)
         )
 
-        raise Exception(
-
-            f"Vision analysis failed: {e}"
-
-        )
+        raise e
